53bfc3f7e6263a2a21e8f420e2e0442b043cae35
xizlt <ivantreada@gmail.com>
2019-04-19 23:30:33 +0300

    1

---
+++ schema.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE readme;
+USE readme;
+
+create table user(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    date_registration DATE 
+    email
+    name
+    password
+    avatar
+    contact
+    lot_id
+    rate_id
+);
