f8fc0d9c82970d65735a590e15cc872ee67f3628
xizlt <ivantreada@gmail.com>
2019-04-21 15:35:02 +0300

    исправления

--- schema.sql
+++ schema.sql
@@ -11,13 +11,8 @@ CREATE TABLE users(
     name VARCHAR(250) NOT NULL,
     password VARCHAR(250) NOT NULL,
     avatar VARCHAR(900),
-    about VARCHAR(900) NULL,
-    post_id INT NULL,
-    messages_id INT NULL
+    about VARCHAR(900) NULL
 );
-
-CREATE INDEX post_id_index ON users(post_id);
-CREATE INDEX messages_id ON users(messages_id);
 
 
 CREATE TABLE posts (
@@ -25,19 +20,19 @@ CREATE TABLE posts (
     create_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     title VARCHAR(250) NOT NULL,
     message VARCHAR(1000) NOT NULL,
-    quote_writer VARCHAR(250) NOT NULL,
-    image VARCHAR(1000) NOT NULL,
-    video VARCHAR(1000) NOT NULL,
-    link VARCHAR(1000) NOT NULL,
+    quote_writer VARCHAR(250) NULL,
+    image VARCHAR(1000) NULL,
+    video VARCHAR(1000) NULL,
+    link VARCHAR(1000) NULL,
     view INT NULL,
     user_id INT NOT NULL,
     type_content INT NOT NULL,
-    hash_tag_id INT NULL
+    hash_tag INT NULL
 );
 
 CREATE INDEX user_id_index ON posts(user_id);
 CREATE INDEX type_content_index ON posts(type_content);
-CREATE INDEX hash_tags_index ON posts(hash_tag_id);
+CREATE INDEX hash_tag_index ON posts(hash_tag);
 CREATE INDEX title_index ON posts(title);
 CREATE INDEX messages_index ON posts(message);
 
@@ -47,25 +42,24 @@ CREATE TABLE comments (
     create_date TIMESTAMP DEFAULT CURRENT_TIME,
     message VARCHAR(1000) NOT NULL,
     user_id INT NOT NULL,
-    message_id INT NOT NULL
+    post_id INT NOT NULL
 );
 
 CREATE INDEX user_id_index ON comments(user_id);
-CREATE INDEX messages_id_index ON comments(message_id);
+CREATE INDEX post_id_index ON comments(post_id);
 
 
 CREATE TABLE likes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NULL,
-    message_id INT NULL
+    post_id INT NULL
 );
 
 CREATE INDEX user_id_index ON likes(user_id);
-CREATE INDEX messages_id_index ON likes(message_id);
+CREATE INDEX messages_id_index ON likes(post_id);
 
 
 CREATE TABLE subscription (
-    id INT AUTO_INCREMENT PRIMARY KEY,
     user_id_on INT NOT NULL,
     user_id_who INT NOT NULL
 );
@@ -73,6 +67,7 @@ CREATE TABLE subscription (
 
 CREATE TABLE messages (
     id INT AUTO_INCREMENT PRIMARY KEY,
+    message VARCHAR(1000) NOT NULL,
     create_date TIMESTAMP DEFAULT CURRENT_TIME,
     sender INT NOT NULL,
     recipient INT NOT NULL
@@ -83,11 +78,9 @@ CREATE INDEX recipient_index ON messages(recipient);
 
 
 CREATE TABLE hash_tags (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name_tag VARCHAR(1000) UNIQUE NOT NULL
+    hash_tag_id INT NULL
 );
 
-CREATE INDEX name_tag_index ON hash_tags(name_tag);
 
 
 CREATE TABLE content (
