42b79dabca4d0af7e0bf54d5f9e099bc1c701e3b
xizlt <ivantreada@gmail.com>
2019-05-18 19:26:01 +0300

    лишнеее

--- test.php
+++ test.php
@@ -1,27 +1,5 @@
 <?php
 require 'bootstrap.php';
-$link = "http://evacademy.ru/bundles/daetheme/frontend/img/logo.png?3";
-/*$w = file_get_contents($p);
-file_put_contents('uploads/' .basename($p) , $w);
-$post_data ['img']= 'uploads/' .basename($p);*/
+$link = "http://devacademy.ru/bundles/daetheme/frontend/img/logo.png?3";
 
 
-/*$urlHeaders = get_headers($link, 1);
-if (!strpos($urlHeaders[0], '200')) {
-     $arr = 'no';
-}else
-{
-    $arr = 'ok';
-}
-*/
-$curl = curl_init();
-curl_setopt_array($curl, array(
-    CURLOPT_URL => $link,
-    CURLOPT_HEADER => true,
-    CURLOPT_RETURNTRANSFER => true,
-    CURLOPT_NOBODY => true));
-$re = !strpos($curl[0], '200');
-curl_close($curl);
-
-
-var_dump($re);
